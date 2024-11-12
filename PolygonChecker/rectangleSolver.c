6141499a422e025ecef3960171d29ba4b88476cf
MAXWELL OMORODION <Maxwell@gmail.com>
2024-11-12 12:11:18 -0500

    header code

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -16,3 +16,25 @@ bool is_rectangle(double points[4][2]) {
 		d[k++] = distance(points[i][0], points[i][1], points[j][0], points[j][1]);
 	}
 }
+
+    double d_max = 0, d_min = d[0];
+    for (int i = 0; i < 6; i++) {
+        if (d[i] > d_max) d_max = d[i];
+        if (d[i] < d_min) d_min = d[i];
+    }
+
+    int count_max = 0, count_min = 0;
+    for (int i = 0; i < 6; i++) {
+        if (d[i] == d_max) count_max++;
+        if (d[i] == d_min) count_min++;
+    }
+
+    return count_max == 2 && count_min == 4;
+}
+
+void calculate_perimeter_area(double points[4][2], double* perimeter, double* area) {
+    double side1 = distance(points[0][0], points[0][1], points[1][0], points[1][1]);
+    double side2 = distance(points[1][0], points[1][1], points[2][0], points[2][1]);
+    *perimeter = 2 * (side1 + side2);
+    *area = side1 * side2;
+}
