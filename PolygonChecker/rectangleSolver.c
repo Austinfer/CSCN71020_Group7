aeda39d4315c90aeff10dd004ca67d830ceb4bfa
MAXWELL OMORODION <Maxwell@gmail.com>
2024-11-12 12:07:28 -0500

    added bool and double

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,3 +1,18 @@
 #include <math.h>
 #include "stdbool.h"
 #include "rectangleSolver.h"
+
+bool is_valid_point(double x, double y) {
+	return true;
+}
+double distance(double x1, double y1, double x2, double y2) {
+	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+bool is_rectangle(double points[4][2]) {
+	double d[6];
+	int k = 0;
+	for (int i = 0; i < 4; j++) {
+		d[k++] = distance(points[i][0], points[i][1], points[j][0], points[j][1]);
+	}
+}
