4f8809c514c5edea5ed517e83ce56b083b285987
AUSTIN FERGUSON <ozzyandrew@hotmail.com>
2024-11-12 11:48:00 -0500

    added onto triangleSolver.h

--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,5 @@
-#pragma once
-char* analyzeTriangle(int side1, int side2, int side3);
+// triangleSolver.h
+
+* analyzeTriangle(int side1, int side2, int side3);
+void calculateTriangleAngles(int side1, int side2, int side3, double* angle1, double* angle2, double* angle3);
+bool is_valid_triangle(int side1, int side2, int side3);
