74eb42dd342ecb5aacbea9d8b658fc72c74a5995
MAXWELL OMORODION <Maxwell@gmail.com>
2024-11-12 12:19:51 -0500

    source file finished

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,21 +1,23 @@
 #include <math.h>
-#include "stdbool.h"
+#include <stdbool.h>
 #include "rectangleSolver.h"
 
 bool is_valid_point(double x, double y) {
-	return true;
+    return true; 
 }
+
 double distance(double x1, double y1, double x2, double y2) {
-	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
 bool is_rectangle(double points[4][2]) {
-	double d[6];
-	int k = 0;
-	for (int i = 0; i < 4; j++) {
-		d[k++] = distance(points[i][0], points[i][1], points[j][0], points[j][1]);
-	}
-}
+    double d[6];
+    int k = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            d[k++] = distance(points[i][0], points[i][1], points[j][0], points[j][1]);
+        }
+    }
 
     double d_max = 0, d_min = d[0];
     for (int i = 0; i < 6; i++) {
